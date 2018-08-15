cc639b42eaa2b0258a62ae1dc925bbd23be10ed5
Hollis <hollis.akins@gmail.com>
2018-08-15 17:10:12 -0400

    setting up CRON job

--- cron_pipeline.sh
+++ cron_pipeline.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# this script is what is executed daily via the CRONTAB
+# it includes a command to cd to /data1 and ls because that is the only way
+# i know that works to undo the lockup from the virtually mounted drives
+
 cmd="cd /data1"
 echo $cmd
 eval $cmd
